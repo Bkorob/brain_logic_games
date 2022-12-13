a9e4eed46d49cb7295b84509a16e65b69b132cf5
Bkorob311 <bkorob311@gmail.com>
2022-12-13 10:36:29 +0300

    апдейт gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -9,11 +9,12 @@ def game():
     num_first = random.randrange(0, 101, counter)
     num_second = random.randrange(0, 101, counter)
     screen_question = (f'{num_first} {num_second}')
-    if num_first > num_second:
-        num_max = num_first
-    else:
+    num_max = num_first
+    if num_second > num_first:
         num_max = num_second
     for i in range(num_max, 0, -1):
         if ((num_first % i == 0) and (num_second % i == 0)):
             screen_answer = i
             return str(screen_answer), screen_question
+
+print(game())
