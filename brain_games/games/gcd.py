ddf2e32ea284ea749f4bab921ce0acd72fde5e9a
Bkorob311 <bkorob311@gmail.com>
2022-12-13 10:31:37 +0300

    апдейт gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,18 +4,16 @@ import random
 QUESTION = 'Find the greatest common divisor of given numbers.'
 
 
-def names():
+def game():
     counter = int(random.choice('235'))
     num_first = random.randrange(0, 101, counter)
     num_second = random.randrange(0, 101, counter)
     screen_question = (f'{num_first} {num_second}')
-    return screen_question
-def game():
     if num_first > num_second:
         num_max = num_first
     else:
         num_max = num_second
-    for i in range(num_max + 1, 0, -1):
+    for i in range(num_max, 0, -1):
         if ((num_first % i == 0) and (num_second % i == 0)):
             screen_answer = i
             return str(screen_answer), screen_question
