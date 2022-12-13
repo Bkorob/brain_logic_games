13ad95f93bcd425e23a7ee9d7a43de3192b901fb
Bkorob311 <bkorob311@gmail.com>
2022-12-13 10:22:16 +0300

    создание brain-progression

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from brain_games.games import progression
+import brain_games.engine
+
+
+def main():
+    brain_games.engine.game_engine(progression)
+
+
+if __name__ == '__main__':
+    main()
