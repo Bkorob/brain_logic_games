6a2e41b10149e56fc6981797baf9462b7501305f
Bkorob311 <bkorob311@gmail.com>
2022-12-19 02:55:00 +0300

    fix calc, gcd

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,7 +4,7 @@ import random
 QUESTION = "What is the result of the expression?"
 
 
-def is_operation(num1, num2, oper):
+def give_operation(num1, num2, oper):
     if oper == '-':
         operation_result = num1 - num2
     elif oper == '+':
@@ -18,7 +18,7 @@ def create_game():
     num_first = random.randint(0, 10)
     num_second = random.randint(0, 10)
     oper = random.choice('-+*')
-    operation_result = is_operation(num_first, num_second, oper)
+    operation_result = give_operation(num_first, num_second, oper)
     # Не понял насчёт "такое определение переменных". А такое?
     screen_ans = str(operation_result)
     screen_q = f'{num_first} {oper} {num_second}'
--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -6,13 +6,12 @@ QUESTION = 'Find the greatest common divisor of given numbers.'
 
 def create_game():
     counter = int(random.choice('235'))
+    # при использовании counter чаще выпадают числа с НОД отличным от единицы
     num_first = random.randrange(0, 101, counter)
     num_second = random.randrange(0, 101, counter)
     screen_q = (f'{num_first} {num_second}')
-    num_max = num_first
-    if num_second > num_first:
-        num_max = num_second
-    for i in range(num_max, 0, -1):
+    num_max = max(num_first, num_second)
+    for i in range(0, num_max + 1):
         if ((num_first % i == 0) and (num_second % i == 0)):
             screen_ans = i
             return str(screen_ans), screen_q
