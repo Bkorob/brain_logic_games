dee4af7a26ff17190bd56390adf89b9943346633
Bkorob311 <bkorob311@gmail.com>
2022-12-19 02:42:19 +0300

    fix calc, even

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,9 +3,10 @@ import random
 
 QUESTION = "What is the result of the expression?"
 
+
 def is_operation(num1, num2, oper):
     if oper == '-':
-        operation_result =  num1- num2
+        operation_result = num1 - num2
     elif oper == '+':
         operation_result = num1 + num2
     elif oper == '*':
@@ -17,7 +18,7 @@ def create_game():
     num_first = random.randint(0, 10)
     num_second = random.randint(0, 10)
     oper = random.choice('-+*')
-    operation_result = is_operation(num_first, num_second, oper) 
+    operation_result = is_operation(num_first, num_second, oper)
     # Не понял насчёт "такое определение переменных". А такое?
     screen_ans = str(operation_result)
     screen_q = f'{num_first} {oper} {num_second}'
--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,13 +4,17 @@ import random
 QUESTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def create_game():
-    number = random.randint(0, 100)
-    result = ''
-    screen_q = str(number)
+def is_even(number):
     if number % 2 == 0:
         result = 'yes'
     else:
         result = 'no'
+    return result
+
+
+def create_game():
+    number = random.randint(0, 100)
+    result = is_even(number)
+    screen_q = number
     screen_ans = str(result)
     return screen_ans, screen_q
