0a22edd2a94cd978d870eba6a413648431a47cf3
Bkorob311 <bkorob311@gmail.com>
2022-12-19 03:11:31 +0300

    fix prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,9 +5,12 @@ QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def create_game():
-    num = random.randint(2, 50)
+    num = random.randint(0, 50)
     screen_q = num
-    for i in range(int(num / 2), 1, - 1):
+    if num <= 2:
+        screen_ans = 'no'
+        return screen_ans, screen_q
+    for i in range(int(num / 2), 0, - 1):
         if num % i == 0:
             screen_ans = 'no'
             return screen_ans, screen_q
