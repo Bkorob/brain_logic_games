b53dff701e448792fb0d0e7e8133631f3d6dff68
Bkorob311 <bkorob311@gmail.com>
2022-12-19 05:20:08 +0300

    фикс brain_prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,15 +4,21 @@ import random
 QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def create_game():
-    num = random.randint(0, 50)
-    screen_question = num
+def is_prime(num):
     if num <= 1:
         screen_answer = 'no'
-        return screen_answer, screen_question
+        return screen_answer
     for i in range(int(num / 2), 1, - 1):
         if num % i == 0:
             screen_answer = 'no'
-            return screen_answer, screen_question
+            return screen_answer
     screen_answer = 'yes'
+    return screen_answer 
+
+
+def create_game():
+    num = random.randint(0, 50)
+    screen_question = num
+    screen_answer = is_prime(num)
     return screen_answer, screen_question
+print(create_game())
