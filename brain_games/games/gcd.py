0facc64d34d7d93ce9413ecb242fc79c666676cd
Bkorob311 <bkorob311@gmail.com>
2022-12-21 15:33:27 +0300

    s

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -9,8 +9,8 @@ def generate_round():
     num_first = random.randrange(1, 101, counter)
     num_second = random.randrange(1, 101, counter)
     question = (f'{num_first} {num_second}')
-    num_max = max(num_first, num_second)
-    for i in range(num_max, 0, - 1):
+    num_min = min(num_first, num_second)
+    for i in range(num_min, 0, - 1):
         if ((num_first % i == 0) and (num_second % i == 0)):
             correct_answer = i
-            return str(correct_answer), question
+        return str(correct_answer), question
