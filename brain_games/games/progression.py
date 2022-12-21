b3bb3ec089d4c03500c9c73d32b8eb32a0526672
Bkorob311 <bkorob311@gmail.com>
2022-12-21 14:43:25 +0300

    fix bug

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,14 +4,15 @@ import random
 RULE = "What number is missing in the progression?"
 
 
-def create_progression():
-    counter = int(random.choice('235'))
-    question_lst = list(range(2, 26, counter))
+def create_progression(end_prog, step_prog):
+    question_lst = list(range(0, end_prog, step_prog))
     return question_lst
 
 
 def generate_round():
-    question_lst = create_progression()
+    end_prog = random.randint(26, 50)
+    step_prog = int(random.choice('235'))
+    question_lst = create_progression(end_prog, step_prog)
     lost_num_index = random.randint(0, len(question_lst) - 1)
     lost_num = question_lst[lost_num_index]
     question_lst[lost_num_index] = ".."
