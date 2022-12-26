b59e8afc33f8200750deac7372782192bafc6132
Bkorob311 <bkorob311@gmail.com>
2022-12-26 21:30:08 +0300

    fix other bug

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -12,10 +12,10 @@ def generate_round():
     start_prog = random.randint(0, 20)
     end_prog = random.randint(46, 50)
     step_prog = int(random.choice('235'))
-    question_lst = create_progression(start_prog, end_prog, step_prog)
-    lost_num_index = random.randint(0, len(question_lst) - 1)
-    lost_num = question_lst[lost_num_index]
-    question_lst[lost_num_index] = ".."
-    question = ' '.join(str(i) for i in question_lst)
+    progression = create_progression(start_prog, end_prog, step_prog)
+    lost_num_index = random.randint(0, len(progression) - 1)
+    lost_num = progression[lost_num_index]
+    progression[lost_num_index] = ".."
+    question = ' '.join(str(i) for i in progression)
     correct_answer = str(lost_num)
     return correct_answer, question
