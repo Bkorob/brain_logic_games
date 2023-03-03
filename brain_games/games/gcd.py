769347fab310713986fb45ec9a268004415fc213
Bkorob311 <bkorob311@gmail.com>
2023-03-03 15:03:15 +0300

    Revert "fix other bug"
    This reverts commit b59e8afc33f8200750deac7372782192bafc6132.

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -11,6 +11,8 @@ def generate_round():
     question = (f'{num_first} {num_second}')
     num_min = min(num_first, num_second)
     for i in range(num_min, 0, -1):
+        # Эта переменная нужна, т.к. если if не выполняется,
+        # без неё цикл падает в ошибку (комментарий уберу в следующем рефе).
         if ((num_first % i == 0) and (num_second % i == 0)):
             correct_answer = i
             return str(correct_answer), question
--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,19 +4,20 @@ import random
 RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def is_prime(question):
-    if question <= 1:
+def is_prime(num):
+    if num <= 1:
         return False
-    for i in range(int(question / 2), 1, - 1):
-        if question % i == 0:
+    for i in range(int(num / 2), 1, - 1):
+        if num % i == 0:
             return False
     return True
 
 
 def generate_round():
-    question = random.randint(0, 50)
-    if is_prime(question):
+    num = random.randint(0, 50)
+    question = num
+    if is_prime(num):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
-    return str(correct_answer), question
+    return correct_answer, question
